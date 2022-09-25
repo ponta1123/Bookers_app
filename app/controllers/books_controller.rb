2deb3d29d5a0c94e1f2392cbd55dbe86de05c310
EC2 Default User <ec2-user@ip-172-31-10-121.ap-northeast-1.compute.internal>
2022-09-25 13:25:24 +0000

    [Add] New bookで空の状態で投稿したらエラー表記追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,15 +4,19 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-
-    book.save
-
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+     redirect_to book_path(@book.id)
+    else
+     @books = Book.all
+     render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -24,9 +28,9 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
@@ -37,7 +41,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-   params.permit(:title, :body)
+   params.require(:book).permit(:title, :body)
   end
 
 end
